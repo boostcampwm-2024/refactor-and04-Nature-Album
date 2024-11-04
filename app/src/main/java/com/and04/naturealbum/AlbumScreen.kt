0b4ba8fd490b3aad3c6ac660febd686ecd7e2aad
Yujin Jeong <yujin45@sookmyung.ac.kr>
2024-11-04 21:14:15 +0900

    [Design] AlbumGrid Composable UI

---
+++ app/src/main/java/com/and04/naturealbum/AlbumScreen.kt
@@ -0,0 +1,249 @@
+package com.and04.naturealbum
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import com.and04.naturealbum.ui.theme.NatureAlbumTheme
+
+data class Album(
+    val id: Int,
+    val label: Label,
+    val photoDetail: PhotoDetail
+)
+
+data class Label(
+    val id: Int,
+    val backgroundColor: Color,
+    val name: String
+)
+
+data class PhotoDetail(
+    val id: Int,
+    val imageResId: Int,
+    val description: String
+)
+
+
+fun getDummyAlbums(): List<Album> {
+    val label1 = Label(1, Color(0xFFE57373), "식빵")
+    val label2 = Label(2, Color(0xFF81C784), "고양이")
+    val photo1 = PhotoDetail(1, R.drawable.sample_image_01, "밥 먹자")
+    val photo2 = PhotoDetail(2, R.drawable.sample_image_02, "고양이")
+    return listOf(
+        Album(1, label1, photo1),
+        Album(2, label2, photo2),
+        Album(3, label1, photo1),
+        Album(4, label2, photo2)
+    )
+}
+
+@Composable
+fun CustomLabel(
+    text: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+
+    val calculatedTextColor = if (backgroundColor.luminance() > 0.5f) Color.Black else Color.White
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .background(color = backgroundColor, shape = MaterialTheme.shapes.large)
+            .clip(RoundedCornerShape(50))
+            .fillMaxWidth(0.8f)
+    ) {
+        Text(
+            text = text,
+            color = calculatedTextColor,
+            fontSize = 14.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+    }
+}
+
+
+@Composable
+fun AlbumItem(album: Album, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CustomLabel(text = album.label.name, backgroundColor = album.label.backgroundColor)
+        Spacer(modifier = Modifier.height(10.dp))
+        Image(
+            painter = painterResource(id = album.photoDetail.imageResId),
+            contentDescription = album.photoDetail.description,
+            modifier = Modifier
+                .wrapContentSize(Alignment.Center)
+                .aspectRatio(1f)
+                .clip(MaterialTheme.shapes.medium),
+            contentScale = ContentScale.Crop
+        )
+
+    }
+}
+
+@Composable
+fun AlbumGrid(albums: List<Album>) {
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 36.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(32.dp)
+    ) {
+        itemsIndexed(albums.chunked(2)) { _, rowItems ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(40.dp)
+            ) {
+                for (album in rowItems) {
+                    AlbumItem(album = album, modifier = Modifier.weight(1f))
+                }
+                if (rowItems.size < 2) {
+                    Spacer(modifier = Modifier.weight(1f))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AlbumScreen(albums: List<Album>) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        AlbumGrid(albums = albums)
+    }
+}
+
+
+@Preview(
+    name = "CustomLabel Light Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "CustomLabel Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun CustomLabelPreview() {
+    NatureAlbumTheme() {
+        Surface {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                CustomLabel(text = "고양이", backgroundColor = Color(0xFFE57373))
+                CustomLabel(text = "강아지", backgroundColor = Color(0xFFD1C4E9))
+                CustomLabel(text = "해오라기", backgroundColor = Color(0xFFC5E1A5))
+            }
+        }
+    }
+
+}
+
+
+@Preview(
+    name = "AlbumItem Light Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "AlbumItem Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun AlbumItemPreview() {
+    NatureAlbumTheme() {
+        Surface {
+            val album = Album(
+                id = 1,
+                label = Label(id = 1, backgroundColor = Color(0xFFE57373), name = "고양이"),
+                photoDetail = PhotoDetail(
+                    id = 1,
+                    imageResId = R.drawable.sample_image_01,
+                    description = "고양고양이"
+                )
+            )
+            AlbumItem(album = album)
+        }
+    }
+}
+
+@Preview(
+    name = "AlbumGrid Light Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "AlbumGrid Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun AlbumGridPreview() {
+    NatureAlbumTheme() {
+        Surface {
+            val albums = remember { getDummyAlbums() }
+            AlbumGrid(albums = albums)
+        }
+    }
+}
+
+@Preview(
+    name = "AlbumScreen Light Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+@Preview(
+    name = "AlbumScreen Dark Mode",
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
+@Composable
+fun AlbumScreenPreview() {
+    NatureAlbumTheme {
+        Surface {
+            val albums = remember { getDummyAlbums() }
+            AlbumScreen(albums = albums)
+        }
+    }
+}
