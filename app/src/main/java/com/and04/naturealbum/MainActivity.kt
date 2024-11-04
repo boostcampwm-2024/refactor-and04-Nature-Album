ef4d1b652e87f73b868179ea25232703065f8555
kim4152 <andykimjhjh@gmail.com>
2024-11-04 15:48:21 +0900

    [Design] Material3 theme 적용

--- app/src/main/java/com/and04/naturealbum/MainActivity.kt
+++ app/src/main/java/com/and04/naturealbum/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.and04.naturealbum
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -34,11 +38,15 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.primary,
+        style = MaterialTheme.typography.headlineSmall
     )
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
 @Composable
 fun GreetingPreview() {
     NatureAlbumTheme {
