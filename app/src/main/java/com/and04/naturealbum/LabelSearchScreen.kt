e39c9eb260f28f20661630170b7ae2852fa96329
kimdoyun98 <rladlsxo1250@gmail.com>
2024-11-05 15:30:44 +0900

    [Design] 1차 라벨 검색 UI 구현

---
+++ app/src/main/java/com/and04/naturealbum/LabelSearchScreen.kt
@@ -0,0 +1,138 @@
+package com.and04.naturealbum
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LabelSearchScreen() {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+//                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+//                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+//                    titleContentColor = MaterialTheme.colorScheme.primary,
+//                ),
+                title = {
+                    Text(
+                        stringResource(R.string.label_search_title_topbar),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        fontFamily = FontFamily.Serif,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            )
+        }
+    ) { innerPadding ->
+        SearchContent(innerPadding)
+    }
+}
+
+@Composable
+fun SearchContent(innerPadding: PaddingValues) {
+    var query by rememberSaveable { mutableStateOf("") }
+    val data = listOf("고양이", "강아지", "장수말벌")
+
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize()
+    ) {
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = query,
+            onValueChange = { query = it },
+            placeholder = { Text(stringResource(R.string.label_search_label_search)) },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                errorContainerColor = Color.Transparent
+            )
+        )
+
+        Text(
+            modifier = Modifier.padding(12.dp),
+            text = stringResource(R.string.label_search_select_create),
+            style = MaterialTheme.typography.bodyMedium
+        )
+
+        LazyColumn {
+            items(data) { label ->
+                AssistChipList(title = label, color = "")
+            }
+        }
+
+        Row(
+            modifier = Modifier.padding(start = 12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(stringResource(R.string.label_search_create))
+            Spacer(Modifier.size(4.dp))
+            AssistChip(
+                onClick = { },
+                label = { Text(query) },
+            )
+        }
+    }
+}
+
+@Composable
+fun AssistChipList(
+    title: String,
+    color: String
+) {
+    AssistChip(
+        modifier = Modifier
+            .padding(start = 12.dp),
+        onClick = { },
+        label = { Text(title) },
+    )
+
+    Spacer(
+        modifier = Modifier
+            .height(0.5.dp)
+            .fillMaxWidth()
+            .background(Color.Gray)
+    )
+}
+
+@Preview
+@Composable
+fun PreviewFunc() {
+    LabelSearchScreen()
+}
