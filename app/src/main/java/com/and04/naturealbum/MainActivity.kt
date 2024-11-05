841ec7be3dae86c325bb3701a12e5a8c900c7520
lh99j <grand2181@gmail.com>
2024-11-05 18:28:59 +0900

    [Chore] 사용하지 않는 코드 제거
    Co-authored-by: Yujin Jeong <yujin45@sookmyung.ac.kr>

--- app/src/main/java/com/and04/naturealbum/MainActivity.kt
+++ app/src/main/java/com/and04/naturealbum/MainActivity.kt
@@ -1,65 +1,13 @@
 package com.and04.naturealbum
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.room.Room
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         val db = AppDatabase.getDatabase(applicationContext)
-        getAll(db)
-
-//        val onclick = { addUser(userDao) }
-
-        setContent {
-//            TestButton(onclick)
-//            GetButton {
-//                Log.d("Room DB test", "${userDao.getAll()}")
-//                userDao.getAll()
-//            }
-        }
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-    }
-}
-
-private fun getAll(db: AppDatabase) = CoroutineScope(Dispatchers.IO).launch {
-    val userDao = db.userDao()
-    val users: List<User> = userDao.getAll()
-    Log.d("MainActivity", "onCreate: $users")
-}
-
-private fun addUser(userDao: UserDao){
-    val user = User(1, "lh99j", "hyungjun")
-    userDao.insertAll(user)
-}
-
-
-@Composable
-fun TestButton(onClick: () -> Unit) {
-    Button(onClick = onClick){
-        Text("test")
-    }
-}
-
-@Composable
-fun GetButton(onClick: () -> Unit){
-    Button(onClick = onClick){
-        Text("Get")
     }
 }
