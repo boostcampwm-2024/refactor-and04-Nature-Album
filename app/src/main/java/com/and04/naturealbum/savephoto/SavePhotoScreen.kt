463f689c850eef45ac63a70fce22b6b340e202df
Jinhwan_ideapad_ubuntu <xsest55@ajou.ac.kr>
2024-11-05 20:47:21 +0900

    [Design] Button with icon

--- app/src/main/java/com/and04/naturealbum/savephoto/SavePhotoScreen.kt
+++ app/src/main/java/com/and04/naturealbum/savephoto/SavePhotoScreen.kt
@@ -4,15 +4,19 @@ import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Create
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
@@ -25,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
@@ -64,15 +69,56 @@ fun SavePhotoScreen(
                 Text("대표 이미지로 설정하기")
             }
             Row(
-                modifier = modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(30.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                Button(onClick = {}, modifier = modifier) {
-                    Text("취소")
+
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(16.dp),
+                    modifier = modifier.weight(1f),
+                    contentPadding = PaddingValues(horizontal = 12.dp, vertical = 10.dp)
+                ) {
+                    Row(
+                        modifier = modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            modifier = modifier.size(24.dp),
+                            imageVector = Icons.Default.Close,
+                            contentDescription = null
+                        )
+                        Text(
+                            text = "취소",
+                            textAlign = TextAlign.Center,
+                            modifier = modifier.weight(1f)
+                        )
+                    }
                 }
-                Button(onClick = {}, modifier = modifier) {
-                    Text("저장")
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(16.dp),
+                    modifier = modifier.weight(1f),
+                    contentPadding = PaddingValues(horizontal = 12.dp, vertical = 10.dp)
+                ) {
+                    Row(
+                        modifier = modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            modifier = modifier.size(24.dp),
+                            imageVector = Icons.Outlined.Create,
+                            contentDescription = null
+                        )
+                        Text(
+                            text = "저장",
+                            textAlign = TextAlign.Center,
+                            modifier = modifier.weight(1f)
+                        )
+                    }
                 }
             }
         }
