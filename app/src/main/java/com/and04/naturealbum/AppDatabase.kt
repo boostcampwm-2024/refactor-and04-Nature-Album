a1bcdae8976de73f06836fd797eefdcf26986f16
lh99j <grand2181@gmail.com>
2024-11-05 14:36:23 +0900

    [Feat] Room DB 예시 코드 적용

---
+++ app/src/main/java/com/and04/naturealbum/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.and04.naturealbum
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
--- app/src/main/java/com/and04/naturealbum/MainActivity.kt
+++ app/src/main/java/com/and04/naturealbum/MainActivity.kt
@@ -1,55 +1,56 @@
 package com.and04.naturealbum
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.and04.naturealbum.ui.theme.NatureAlbumTheme
+import androidx.room.Room
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).build()
+
+        val userDao = db.userDao()
+        val users: List<User> = userDao.getAll()
+
+        val onclick = { addUser(userDao) }
+
         setContent {
-            NatureAlbumTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            TestButton(onclick)
+            GetButton {
+                Log.d("Room DB test", "${userDao.getAll()}")
+                userDao.getAll()
             }
         }
+
+    }
+}
+
+private fun addUser(userDao: UserDao){
+    val user = User(1, "lh99j", "hyungjun")
+    userDao.insertAll(user)
+}
+
+
+@Composable
+fun TestButton(onClick: () -> Unit) {
+    Button(onClick = onClick){
+        Text("test")
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.primary,
-        style = MaterialTheme.typography.headlineSmall
-    )
-
-}
-
-@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
-@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
-@Composable
-fun GreetingPreview() {
-    NatureAlbumTheme {
-        Greeting("Android")
+fun GetButton(onClick: () -> Unit){
+    Button(onClick = onClick){
+        Text("Get")
     }
 }
