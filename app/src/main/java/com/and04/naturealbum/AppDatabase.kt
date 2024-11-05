ef3534423fc9927ac5ebdc1a4cc607fc39487fb8
lh99j <grand2181@gmail.com>
2024-11-05 15:22:17 +0900

    [Fix] main thread 로 실행하던 버그 IO thread로 변경하여 해결

--- app/src/main/java/com/and04/naturealbum/AppDatabase.kt
+++ app/src/main/java/com/and04/naturealbum/AppDatabase.kt
@@ -1,9 +1,24 @@
 package com.and04.naturealbum
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class], version = 1)
+@Database(entities = [User::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun userDao(): UserDao
+
+    companion object {
+        @Volatile
+        private var Instance: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, AppDatabase::class.java, "nature_album_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
 }
--- app/src/main/java/com/and04/naturealbum/MainActivity.kt
+++ app/src/main/java/com/and04/naturealbum/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.and04.naturealbum
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -9,30 +10,38 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.room.Room
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val db = Room.databaseBuilder(
-            applicationContext,
-            AppDatabase::class.java, "database-name"
-        ).build()
+        val db = AppDatabase.getDatabase(applicationContext)
+        getAll(db)
 
-        val userDao = db.userDao()
-        val users: List<User> = userDao.getAll()
-
-        val onclick = { addUser(userDao) }
+//        val onclick = { addUser(userDao) }
 
         setContent {
-            TestButton(onclick)
-            GetButton {
-                Log.d("Room DB test", "${userDao.getAll()}")
-                userDao.getAll()
-            }
+//            TestButton(onclick)
+//            GetButton {
+//                Log.d("Room DB test", "${userDao.getAll()}")
+//                userDao.getAll()
+//            }
         }
 
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
+
+private fun getAll(db: AppDatabase) = CoroutineScope(Dispatchers.IO).launch {
+    val userDao = db.userDao()
+    val users: List<User> = userDao.getAll()
+    Log.d("MainActivity", "onCreate: $users")
 }
 
 private fun addUser(userDao: UserDao){
