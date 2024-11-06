13e980b22f81e36e05eba98c9eb9f832c1578ae5
kimdoyun98 <rladlsxo1250@gmail.com>
2024-11-06 18:27:46 +0900

    [Fix] Room DB 사용 IO스레드로 변경

--- app/src/main/java/com/and04/naturealbum/ui/labelsearch/LabelSearchViewModel.kt
+++ app/src/main/java/com/and04/naturealbum/ui/labelsearch/LabelSearchViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.and04.naturealbum.data.repository.DataRepository
 import com.and04.naturealbum.data.room.Label
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -22,7 +23,7 @@ class LabelSearchViewModel @Inject constructor(
     }
 
     private fun fetchLabels(){
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             _labels.emit(repository.getLabels())
         }
     }
