f6682432c93e2e4303ec2138d94271b01cab4113
lh99j <grand2181@gmail.com>
2024-11-06 16:15:05 +0900

    [Refactor] 콜백의 값을 명시적으로 사용하여 it 제거

--- app/src/main/java/com/and04/naturealbum/data/room/AppDatabase.kt
+++ app/src/main/java/com/and04/naturealbum/data/room/AppDatabase.kt
@@ -23,7 +23,9 @@ abstract class AppDatabase : RoomDatabase() {
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, AppDatabase::class.java, "nature_album_database")
                     .build()
-                    .also { Instance = it }
+                    .also { appDataBase ->
+                        Instance = appDataBase
+                    }
             }
         }
     }
