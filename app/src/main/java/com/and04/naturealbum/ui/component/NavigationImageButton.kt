9a2dee7f716b1dc803c0dfac21347e52f51fbfe0
lh99j <grand2181@gmail.com>
2024-11-12 14:35:25 +0900

    [Refactor] 사용하지 않는 컴포저블 함수 제거 및 컴포넌트 이름 변경

--- app/src/main/java/com/and04/naturealbum/ui/component/NavigationImageButton.kt
+++ app/src/main/java/com/and04/naturealbum/ui/component/NavigationImageButton.kt
@@ -6,32 +6,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-
-@Composable
-fun RoundedShapeButton(text: String, modifier: Modifier, onClick: () -> Unit) {
-    Button(
-        onClick = onClick,
-        modifier = modifier,
-        shape = RoundedCornerShape(10)
-    ) {
-        Text(
-            text = text,
-            modifier = Modifier
-                .padding(top = 24.dp)
-                .align(Alignment.Top)
-        )
-    }
-}
 
 @Composable
 fun NavigationImageButton(
