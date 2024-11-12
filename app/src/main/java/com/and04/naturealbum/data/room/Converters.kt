ca01a305e024fc0b4062e06ab85c36c6dd4a65d9
Jinhwan_ideapad_ubuntu <xsest55@ajou.ac.kr>
2024-11-12 21:22:19 +0900

    [Fix] LocalDateTime converter

--- app/src/main/java/com/and04/naturealbum/data/room/Converters.kt
+++ app/src/main/java/com/and04/naturealbum/data/room/Converters.kt
@@ -2,10 +2,8 @@ package com.and04.naturealbum.data.room
 
 import androidx.room.TypeConverter
 import java.time.LocalDateTime
-import java.time.OffsetDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
-
 
 class Converters {
     @TypeConverter
@@ -13,11 +11,13 @@ class Converters {
         return dateTime?.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
     }
 
-    // TODO: UTC -> Local 매핑되는지 확인 필요
     @TypeConverter
     fun stringToLocalDateTime(dateTimeString: String?): LocalDateTime? {
         return dateTimeString?.let { timeString ->
-            OffsetDateTime.parse(timeString).atZoneSameInstant(ZoneId.systemDefault())
+            LocalDateTime
+                .parse(timeString, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+                .atZone(ZoneId.of("UTC"))
+                .withZoneSameInstant(ZoneId.systemDefault())
                 .toLocalDateTime()
         }
     }
