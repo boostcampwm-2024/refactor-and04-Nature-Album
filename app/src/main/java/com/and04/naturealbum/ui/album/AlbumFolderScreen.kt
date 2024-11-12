22bc6cfb375f6311b924019180e0a757666a6c0d
kim4152 <andykimjhjh@gmail.com>
2024-11-12 11:19:45 +0900

    [Fix] 이미지 중앙 배치 문제 해결

--- app/src/main/java/com/and04/naturealbum/ui/album/AlbumFolderScreen.kt
+++ app/src/main/java/com/and04/naturealbum/ui/album/AlbumFolderScreen.kt
@@ -1,6 +1,11 @@
 package com.and04.naturealbum.ui.album
 
+import android.graphics.Color.parseColor
 import android.util.Log
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,8 +17,8 @@ import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
@@ -24,6 +29,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,6 +37,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -39,6 +47,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil3.compose.AsyncImage
+import com.and04.naturealbum.R
 import com.and04.naturealbum.data.room.PhotoDetail
 import com.and04.naturealbum.ui.component.AlbumLabel
 import com.and04.naturealbum.ui.component.MyTopAppBar
@@ -51,10 +60,12 @@ fun AlbumFolderScreen(
     onPhotoClick: (Int) -> Unit = {},
     albumFolderViewModel: AlbumFolderViewModel = hiltViewModel(),
 ) {
-    val editMode = remember { mutableStateOf(false) }
-    val checkList = remember { mutableStateOf(setOf<TmpItem>()) }
-    if (!editMode.value) checkList.value = setOf()
     albumFolderViewModel.loadFolderData(selectedAlbumLabel)
+
+    val editMode = remember { mutableStateOf(false) }
+    val checkList = remember { mutableStateOf(setOf<PhotoDetail>()) }
+    if (!editMode.value) checkList.value = setOf()
+
     Scaffold(topBar = { MyTopAppBar() }) { innerPadding ->
         ItemContainer(
             innerPaddingValues = innerPadding,
@@ -75,7 +86,7 @@ private fun ItemContainer(
     innerPaddingValues: PaddingValues,
     onPhotoClick: (Int) -> Unit,
     editMode: MutableState<Boolean>,
-    checkList: MutableState<Set<TmpItem>>,
+    checkList: MutableState<Set<PhotoDetail>>,
     albumFolderViewModel: AlbumFolderViewModel = hiltViewModel(),
 ) {
     val uiState = albumFolderViewModel.uiState.collectAsState()
@@ -109,7 +120,6 @@ private fun ItemContainer(
                     )
                     Box(
                         modifier = Modifier.fillMaxSize(),
-                        contentAlignment = Alignment.Center
                     ) {
                         LazyVerticalStaggeredGrid(
                             columns = StaggeredGridCells.Fixed(2),
@@ -153,7 +163,7 @@ private fun Item(
     item: PhotoDetail,
     onPhotoClick: (Int) -> Unit,
     editMode: MutableState<Boolean>,
-    checkList: MutableState<Set<TmpItem>>,
+    checkList: MutableState<Set<PhotoDetail>>,
 ) {
     var isSelected by remember { mutableStateOf(false) }
     if (!editMode.value && isSelected) {
