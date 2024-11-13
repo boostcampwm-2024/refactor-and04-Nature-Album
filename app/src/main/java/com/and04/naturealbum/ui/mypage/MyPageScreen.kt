73b3c92e6848e789d33c9d6d11014c6e310f58bd
lh99j <grand2181@gmail.com>
2024-11-13 23:01:31 +0900

    [Design] 더미 데이터를 통해 마이 페이지 UI 뼈대 구현

--- app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageScreen.kt
+++ app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageScreen.kt
@@ -1,10 +1,39 @@
 package com.and04.naturealbum.ui.mypage
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil3.compose.AsyncImage
+import com.and04.naturealbum.R
+import com.and04.naturealbum.ui.component.MyTopAppBar
+import com.and04.naturealbum.ui.theme.NatureAlbumTheme
 
 @Composable
 fun MyPageScreen(
@@ -12,8 +41,119 @@ fun MyPageScreen(
 ) {
     val context = LocalContext.current
 
-    Button(onClick = {
-        myPageViewModel.signInWithGoogle(context)
+    Scaffold(topBar = {
+        MyTopAppBar(
+            navigationIcon = {
+                IconButton(onClick = { /* TODO */ }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.my_page_arrow_back_icon_content_description)
+                    )
+                }
+            }
+        )
+    }) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(32.dp)
+        ) {
+            UserProfileContent("", "")
+            LoginContent({})
+        }
     }
-    ) { Text("Test Button") }
+}
+
+@Composable
+fun MyPageScreen(dummyClick: () -> Unit) {
+    Scaffold(topBar = {
+        MyTopAppBar(
+            navigationIcon = {
+                IconButton(onClick = { /* TODO */ }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.my_page_arrow_back_icon_content_description)
+                    )
+                }
+            }
+        )
+    }) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(32.dp)
+        ) {
+            UserProfileContent("", "")
+            LoginContent({})
+        }
+    }
+}
+
+@Composable
+fun UserProfileContent(uri: String, email: String) {
+    UserProfileImage(
+        uri = uri,
+        modifier = Modifier
+            .fillMaxHeight(0.2f)
+            .aspectRatio(1f)
+    )
+
+    Text(
+        text = email.ifEmpty { stringResource(R.string.my_page_default_user_email) },
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun UserProfileImage(uri: String, modifier: Modifier) {
+    if (uri.isEmpty()) {
+        Image(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = stringResource(R.string.my_page_user_profile_image),
+            modifier = modifier
+        )
+    } else {
+        AsyncImage(
+            model = uri,
+            contentDescription = stringResource(R.string.my_page_user_profile_image),
+            modifier = modifier.clip(CircleShape)
+        )
+    }
+}
+
+@Composable
+fun LoginContent(loginHandle: () -> Unit) {
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.my_page_login_txt),
+            textAlign = TextAlign.Left
+        )
+
+        Button(
+            onClick = { loginHandle() },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(30)
+        ) {
+            Text(text = stringResource(R.string.my_page_google_login_btn))
+        }
+    }
+}
+
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
+@Composable
+private fun MyPageScreenPreview() {
+    NatureAlbumTheme {
+        MyPageScreen({})
+    }
 }
