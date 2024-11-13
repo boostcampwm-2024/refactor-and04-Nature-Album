b28893c0f54f2004524fa5d756a70eab14e44c39
lh99j <grand2181@gmail.com>
2024-11-14 00:09:30 +0900

    [Feat] User 정보 관리 Singleton object UserManager 생성

---
+++ app/src/main/java/com/and04/naturealbum/ui/mypage/UserManager.kt
@@ -0,0 +1,19 @@
+package com.and04.naturealbum.ui.mypage
+
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+object UserManager {
+    private val auth = Firebase.auth
+
+    fun isSignIn() = auth.currentUser != null
+
+    fun getUser(): FirebaseUser? {
+        return auth.currentUser
+    }
+
+    fun getNotNullSignInUser(): FirebaseUser {
+        return auth.currentUser!!
+    }
+}
