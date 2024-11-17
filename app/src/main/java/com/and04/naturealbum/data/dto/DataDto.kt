6d450bf456d6562d9328ddbc5eb1cabdf8837c29
lh99j <grand2181@gmail.com>
2024-11-17 13:02:54 +0900

    [Fix] AlbumScreen Preview 버그 해결

--- app/src/main/java/com/and04/naturealbum/data/dto/DataDto.kt
+++ app/src/main/java/com/and04/naturealbum/data/dto/DataDto.kt
@@ -5,4 +5,14 @@ data class AlbumDto(
     val labelName: String,
     val labelBackgroundColor: String,
     val photoDetailUri: String
-)
+) {
+    companion object {
+        fun dummyList(): List<AlbumDto> {
+            return listOf(
+                AlbumDto(0, "강아지", "E57373", ""),
+                AlbumDto(1, "고양이", "D1C4E9", ""),
+                AlbumDto(2, "해오라기", "C5E1A5", "")
+            )
+        }
+    }
+}
--- app/src/main/java/com/and04/naturealbum/ui/album/AlbumScreen.kt
+++ app/src/main/java/com/and04/naturealbum/ui/album/AlbumScreen.kt
@@ -19,23 +19,21 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.and04.naturealbum.data.dto.AlbumDto
 import com.and04.naturealbum.ui.component.AlbumLabel
@@ -46,34 +44,59 @@ import com.and04.naturealbum.utils.gridColumnCount
 import com.and04.naturealbum.utils.toColor
 
 @Composable
-fun AlbumItem(album: AlbumDto, onLabelClick: (Int) -> Unit, modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        AlbumLabel(
-            modifier = Modifier
-                .background(
-                    color = album.labelBackgroundColor.toColor(),
-                    shape = CircleShape
-                )
-                .fillMaxWidth(0.8f)
-                .clickable { onLabelClick(album.labelId) },
-            text = album.labelName,
-            backgroundColor = album.labelBackgroundColor.toColor()
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        AsyncImage(
-            model = album.photoDetailUri.toUri(),
-            contentDescription = album.labelName, // TODO: 해당 description 무엇으로 할지 확정
-            modifier = Modifier
-                .wrapContentSize(Alignment.Center)
-                .aspectRatio(1f)
-                .clip(MaterialTheme.shapes.medium)
-                .clickable { onLabelClick(album.labelId) },
-            contentScale = ContentScale.Crop,
-        )
+fun AlbumScreen(
+    onLabelClick: (Int) -> Unit,
+    onNavigateToMyPage: () -> Unit,
+    viewModel: AlbumViewModel = hiltViewModel(),
+) {
+    val isDataLoaded = rememberSaveable { mutableStateOf(false) }
+    LaunchedEffect(onLabelClick) {
+        if (!isDataLoaded.value) {
+            viewModel.loadAlbums()
+            isDataLoaded.value = true
+        }
+    }
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val albumList by viewModel.albumList.collectAsStateWithLifecycle()
+
+    AlbumScreen(
+        uiState = uiState,
+        albumList = albumList,
+        onLabelClick = onLabelClick,
+        onNavigateToMyPage = onNavigateToMyPage
+    )
+}
+
+@Composable
+fun AlbumScreen(
+    uiState: UiState,
+    albumList: List<AlbumDto>,
+    onLabelClick: (Int) -> Unit,
+    onNavigateToMyPage: () -> Unit,
+) {
+    when (uiState) {
+        is UiState.Loading, UiState.Idle -> {
+            // TODO: loading
+        }
+
+        UiState.Success -> {
+            Scaffold(
+                topBar = { LocalContext.current.GetTopbar { onNavigateToMyPage() } }
+            ) { paddingValues ->
+                Column(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .fillMaxSize()
+                ) {
+                    AlbumGrid(
+                        albums = albumList,
+                        onLabelClick = onLabelClick,
+                        columnCount = LocalContext.current.gridColumnCount(),
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -83,8 +106,6 @@ fun AlbumGrid(
     onLabelClick: (Int) -> Unit,
     columnCount: Int,
 ) {
-
-
     LazyVerticalGrid(
         columns = GridCells.Fixed(columnCount),
         modifier = Modifier.fillMaxWidth(),
@@ -104,97 +125,39 @@ fun AlbumGrid(
 }
 
 @Composable
-fun AlbumScreen(
-    onLabelClick: (Int) -> Unit,
-    viewModel: AlbumViewModel = hiltViewModel(),
-    onNavigateToMyPage: () -> Unit,
-) {
-    val isDataLoaded = rememberSaveable { mutableStateOf(false) }
-    LaunchedEffect(onLabelClick) {
-        if (!isDataLoaded.value) {
-            viewModel.loadAlbums()
-            isDataLoaded.value = true
-        }
-    }
+fun AlbumItem(album: AlbumDto, onLabelClick: (Int) -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AlbumLabel(
+            modifier = Modifier
+                .background(
+                    color = album.labelBackgroundColor.toColor(),
+                    shape = CircleShape
+                )
+                .fillMaxWidth(0.8f)
+                .clickable { onLabelClick(album.labelId) },
+            text = album.labelName,
+            backgroundColor = album.labelBackgroundColor.toColor()
+        )
 
-    val uiState = viewModel.uiState.observeAsState()
-    val albumList by viewModel.albumList.observeAsState()
+        Spacer(modifier = Modifier.height(10.dp))
 
-    when (uiState.value) {
-        is UiState.Loading, UiState.Idle, null -> {
-            // TODO: loading
-        }
-
-        UiState.Success -> {
-            Scaffold(
-                topBar = { LocalContext.current.GetTopbar { onNavigateToMyPage() } }
-            ) { paddingValues ->
-                Column(
-                    modifier = Modifier
-                        .padding(paddingValues)
-                        .fillMaxSize()
-                ) {
-                    albumList?.let { albumList ->
-                        AlbumGrid(
-                            albums = albumList,
-                            onLabelClick = onLabelClick,
-                            columnCount = LocalContext.current.gridColumnCount(),
-                        )
-                    }
-                }
-            }
-        }
+        AsyncImage(
+            model = album.photoDetailUri.toUri(),
+            contentDescription = album.labelName, // TODO: 해당 description 무엇으로 할지 확정
+            modifier = Modifier
+                .wrapContentSize(Alignment.Center)
+                .aspectRatio(1f)
+                .clip(MaterialTheme.shapes.medium)
+                .clickable { onLabelClick(album.labelId) },
+            contentScale = ContentScale.Crop,
+        )
     }
 }
 
-@Preview(
-    name = "AlbumLabel Light Mode",
-    showBackground = true,
-    uiMode = Configuration.UI_MODE_NIGHT_NO
-)
-@Preview(
-    name = "AlbumLabel Dark Mode",
-    showBackground = true,
-    uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-
-@Composable
-fun AlbumLabelPreview() {
-    NatureAlbumTheme {
-        Surface {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                AlbumLabel(
-                    modifier = Modifier
-                        .background(color = Color(0xFFE57373), shape = CircleShape)
-                        .fillMaxWidth(0.8f),
-                    text = "고양이",
-                    backgroundColor = Color(0xFFE57373)
-                )
-                AlbumLabel(
-                    modifier = Modifier
-                        .background(color = Color(0xFFD1C4E9), shape = CircleShape)
-                        .fillMaxWidth(0.8f),
-                    text = "강아지",
-                    backgroundColor = Color(0xFFD1C4E9)
-                )
-                AlbumLabel(
-                    modifier = Modifier
-                        .background(color = Color(0xFFC5E1A5), shape = CircleShape)
-                        .fillMaxWidth(0.8f),
-                    text = "해오라기",
-                    backgroundColor = Color(0xFFC5E1A5)
-                )
-            }
-        }
-    }
-
-}
 
 @Preview(
     name = "AlbumScreen Light Mode",
@@ -209,11 +172,11 @@ fun AlbumLabelPreview() {
 @Composable
 fun AlbumScreenPreview() {
     NatureAlbumTheme {
-        Surface {
-            AlbumScreen(
-                onLabelClick = {},
-                onNavigateToMyPage = {},
-            )
-        }
+        AlbumScreen(
+            uiState = UiState.Success,
+            albumList = AlbumDto.dummyList(),
+            onLabelClick = {},
+            onNavigateToMyPage = {},
+        )
     }
 }
