3151d685541bd34b84b813917296ac06bad7a92b
Jinhwan_ideapad_ubuntu <xsest55@ajou.ac.kr>
2024-11-18 17:11:57 +0900

    [Feat] define clustering key

---
+++ app/src/main/java/com/and04/naturealbum/data/ItemKey.kt
@@ -0,0 +1,18 @@
+package com.and04.naturealbum.data
+
+import com.and04.naturealbum.data.room.PhotoDetail
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.clustering.ClusteringKey
+
+class ItemKey(private val photoDetail: PhotoDetail) : ClusteringKey {
+
+    override fun getPosition(): LatLng = LatLng(photoDetail.latitude, photoDetail.longitude)
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ItemKey) return false
+        return photoDetail.id == other.photoDetail.id
+    }
+
+    override fun hashCode() = photoDetail.id
+}
