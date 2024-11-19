9f0e86955e0260d882e547c57dc88eafbfc20fef
kimdoyun98 <rladlsxo1250@gmail.com>
2024-11-19 17:56:34 +0900

    [Refactor] Stream들 확장함수 use로 close()

--- app/src/main/java/com/and04/naturealbum/utils/ImageConvert.kt
+++ app/src/main/java/com/and04/naturealbum/utils/ImageConvert.kt
@@ -24,20 +24,19 @@ class ImageConvert(
             val imageFile = File(storage, fileName)
             imageFile.createNewFile()
 
-            val fos = FileOutputStream(imageFile)
+            FileOutputStream(imageFile).use { fos ->
+                decodeBitmapFromUri(uri)?.apply {
+                    if (Build.VERSION.SDK_INT >= 30) {
+                        compress(Bitmap.CompressFormat.WEBP_LOSSY, 100, fos)
+                    } else {
+                        compress(Bitmap.CompressFormat.JPEG, 100, fos)
+                    }
 
-            decodeBitmapFromUri(uri)?.apply {
-                if (Build.VERSION.SDK_INT >= 30) {
-                    compress(Bitmap.CompressFormat.WEBP_LOSSY, 100, fos)
-                } else {
-                    compress(Bitmap.CompressFormat.JPEG, 100, fos)
-                }
+                    recycle()
+                } ?: throw NullPointerException()
 
-                recycle()
-            } ?: throw NullPointerException()
-
-            fos.flush()
-            fos.close()
+                fos.flush()
+            }
 
             return ResizePicture(
                 fileName = fileName,
@@ -52,26 +51,27 @@ class ImageConvert(
     }
 
     private fun decodeBitmapFromUri(uri: Uri): Bitmap? {
-        val input = BufferedInputStream(context.contentResolver.openInputStream(uri))
-        input.mark(input.available())
+        BufferedInputStream(
+            context.contentResolver.openInputStream(uri)
+        ).use { input ->
+            input.mark(input.available())
 
-        var bitmap: Bitmap?
+            var bitmap: Bitmap?
 
-        BitmapFactory.Options().run {
-            inJustDecodeBounds = true
-            bitmap = BitmapFactory.decodeStream(input, null, this)
-            input.reset()
+            BitmapFactory.Options().run {
+                inJustDecodeBounds = true
+                bitmap = BitmapFactory.decodeStream(input, null, this)
+                input.reset()
 
-            inSampleSize = calculateInSampleSize(this)
-            inJustDecodeBounds = false
+                inSampleSize = calculateInSampleSize(this)
+                inJustDecodeBounds = false
 
-            bitmap = BitmapFactory.decodeStream(input, null, this)
-            if (bitmap != null) bitmap = rotateImageIfRequired(bitmap = bitmap!!, uri = uri)
+                bitmap = BitmapFactory.decodeStream(input, null, this)
+                if (bitmap != null) bitmap = rotateImageIfRequired(bitmap = bitmap!!, uri = uri)
+            }
+
+            return bitmap
         }
-
-        input.close()
-
-        return bitmap
     }
 
     private fun calculateInSampleSize(options: BitmapFactory.Options): Int {
@@ -86,22 +86,27 @@ class ImageConvert(
                 inSampleSize *= 2
             }
         }
-        Log.e("inSampleSize", inSampleSize.toString())
+
         return inSampleSize
     }
 
     private fun rotateImageIfRequired(bitmap: Bitmap, uri: Uri): Bitmap? {
         val input = context.contentResolver.openInputStream(uri) ?: return null
-        val exif = ExifInterface(input)
+        input.use {
+            val exif = ExifInterface(input)
 
-        val orientation =
-            exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+            val orientation =
+                exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL
+                )
 
-        return when (orientation) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(bitmap, 90)
-            ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(bitmap, 180)
-            ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(bitmap, 270)
-            else -> bitmap
+            return when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(bitmap, 90)
+                ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(bitmap, 180)
+                ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(bitmap, 270)
+                else -> bitmap
+            }
         }
     }
 
