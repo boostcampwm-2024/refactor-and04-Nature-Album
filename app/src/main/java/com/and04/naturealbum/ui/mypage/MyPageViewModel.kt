b7e1af93ed6c0ab3d48e258ceba0c07479b79c70
lh99j <grand2181@gmail.com>
2024-11-19 15:14:52 +0900

    [Feat] 뷰모델에서 친구 리스트 상태 추가

--- app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageViewModel.kt
@@ -3,6 +3,7 @@ package com.and04.naturealbum.ui.mypage
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.and04.naturealbum.data.dto.MyFriend
 import com.and04.naturealbum.ui.savephoto.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,6 +18,9 @@ class MyPageViewModel @Inject constructor(
 ) : ViewModel() {
     private val _uiState = MutableStateFlow<UiState>(setInitUiState())
     val uiState: StateFlow<UiState> = _uiState
+
+    private val _myFriends = MutableStateFlow<List<MyFriend>>(emptyList())
+    val myFriend: StateFlow<List<MyFriend>> = _myFriends
 
     fun signInWithGoogle(context: Context) {
         authenticationManager.signInWithGoogle(context).onEach { response ->
