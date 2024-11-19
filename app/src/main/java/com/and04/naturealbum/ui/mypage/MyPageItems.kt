0baa360a077783b2b4b7c5bd4e4214d595a74ab5
lh99j <grand2181@gmail.com>
2024-11-19 15:21:12 +0900

    [Chore] 사용하지 않는 import 제거

--- app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageItems.kt
+++ app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageItems.kt
@@ -13,8 +13,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
