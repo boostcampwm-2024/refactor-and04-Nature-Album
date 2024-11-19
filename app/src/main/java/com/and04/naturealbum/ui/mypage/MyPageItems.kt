e0fce50555e7012f432870b34f1112effcbd3434
lh99j <grand2181@gmail.com>
2024-11-19 15:15:19 +0900

    [Design] MyPage 친구목록, 친구검색, 알림 UI 구현

---
+++ app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageItems.kt
@@ -0,0 +1,221 @@
+package com.and04.naturealbum.ui.mypage
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.SuggestionChip
+import androidx.compose.material3.SuggestionChipDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.and04.naturealbum.R
+import com.and04.naturealbum.data.dto.MyFriend
+
+@Composable
+fun MyPageSocialList(myFriends: List<MyFriend>) {
+    LazyColumn {
+        items(items = myFriends, key = { myFriend -> myFriend.email }) { myFriend ->
+            MyPageSocialItem(myFriend)
+        }
+    }
+}
+
+@Composable
+fun MyPageSocialItem(myFriend: MyFriend) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .height(40.dp)
+                .clip(CircleShape),
+            model = myFriend.uri,
+            contentDescription = stringResource(R.string.my_page_user_profile_image),
+        )
+
+        Text(text = myFriend.email)
+    }
+
+    HorizontalDivider(thickness = 1.dp)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyPageSearch(myFriends: List<MyFriend>) {
+    var textFieldState by remember { mutableStateOf("") }
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Column(
+        Modifier.fillMaxSize()
+    ) {
+        SearchBar(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally),
+            inputField = {
+                SearchBarDefaults.InputField(
+                    query = textFieldState,
+                    onSearch = { expanded = false },
+                    expanded = expanded,
+                    onExpandedChange = { expanded = it },
+                    placeholder = {
+                        Text(text = stringResource(R.string.my_page_search_hint))
+                    },
+                    leadingIcon = {
+                        Icon(
+                            Icons.Default.Search,
+                            contentDescription = stringResource(R.string.my_page_search_bar_search_icon)
+                        )
+                    },
+                    onQueryChange = { textFieldState = it }
+                )
+            },
+            expanded = expanded,
+            onExpandedChange = { expanded = it },
+        ) {
+            // 검색 결과 리스트
+            RequestedList(myFriends)
+        }
+
+        // 요청된 친구 리스트
+        RequestedList(myFriends)
+    }
+}
+
+@Composable
+fun RequestedList(myFriends: List<MyFriend>) {
+    LazyColumn {
+        items(items = myFriends, key = { myFriend -> myFriend.email }) { myFriend ->
+            RequestedItem(myFriend)
+        }
+    }
+}
+
+@Composable
+fun RequestedItem(myFriend: MyFriend) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .height(40.dp)
+                    .clip(CircleShape),
+                model = myFriend.uri,
+                contentDescription = stringResource(R.string.my_page_user_profile_image),
+            )
+
+            Text(text = myFriend.email)
+        }
+
+
+        SuggestionChip(
+            onClick = { /* TODO */ },
+            label = {
+                val text = if (myFriend.isRequest)
+                    stringResource(R.string.my_page_friend_requested)
+                else
+                    stringResource(R.string.my_page_friend_request)
+
+                Text(text = text)
+            },
+            colors = SuggestionChipDefaults.suggestionChipColors(
+                containerColor = MaterialTheme.colorScheme.tertiary,
+                labelColor = Color.White
+            )
+        )
+    }
+
+    HorizontalDivider(thickness = 1.dp)
+}
+
+@Composable
+fun MyPageAlarm(myAlarms: List<MyFriend>, onDenied: () -> Unit, onAccept: () -> Unit) {
+    LazyColumn {
+        items(items = myAlarms, key = { myFriend -> myFriend.email }) { myFriend ->
+            MyPageAlarmItem(myFriend, onDenied, onAccept)
+        }
+    }
+}
+
+@Composable
+fun MyPageAlarmItem(myFriend: MyFriend, onDenied: () -> Unit, onAccept: () -> Unit) {
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .height(40.dp)
+                    .clip(CircleShape),
+                model = myFriend.uri,
+                contentDescription = stringResource(R.string.my_page_user_profile_image),
+            )
+
+            Text(text = "${myFriend.email}${stringResource(R.string.my_page_alarm_txt)}")
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            Button(onClick = onAccept) {
+                Text(text = stringResource(R.string.my_page_request_accept))
+            }
+
+            Spacer(modifier = Modifier.width(32.dp))
+
+            Button(onClick = onDenied) {
+                Text(text = stringResource(R.string.my_page_request_deny))
+            }
+        }
+    }
+
+    HorizontalDivider(thickness = 1.dp)
+}
--- app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageScreen.kt
+++ app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageScreen.kt
@@ -6,7 +6,9 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,27 +19,38 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Badge
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
 import com.and04.naturealbum.R
+import com.and04.naturealbum.data.dto.MyFriend
 import com.and04.naturealbum.ui.component.PortraitTopAppBar
 import com.and04.naturealbum.ui.savephoto.UiState
 import com.and04.naturealbum.ui.theme.NatureAlbumTheme
@@ -47,22 +60,22 @@ fun MyPageScreen(
     navigateToHome: () -> Unit,
     myPageViewModel: MyPageViewModel = hiltViewModel(),
 ) {
-    val context = LocalContext.current
     val uiState = myPageViewModel.uiState.collectAsStateWithLifecycle()
+    val myFriends = myPageViewModel.myFriend.collectAsStateWithLifecycle()
 
     MyPageScreen(
-        context = context,
         navigateToHome = navigateToHome,
         uiState = uiState,
+        myFriends = myFriends,
         signInWithGoogle = myPageViewModel::signInWithGoogle
     )
 }
 
 @Composable
 fun MyPageScreen(
-    context: Context,
     navigateToHome: () -> Unit,
     uiState: State<UiState>,
+    myFriends: State<List<MyFriend>>,
     signInWithGoogle: (Context) -> Unit,
 ) {
     Scaffold(topBar = {
@@ -77,28 +90,50 @@ fun MyPageScreen(
             }
         )
     }) { innerPadding ->
-        Column(
+        MyPageContent(
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(horizontal = 16.dp)
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(32.dp)
-        ) {
-            when (uiState.value) {
-                is UiState.Success -> {
-                    val user = UserManager.getUser()
-                    val email = user?.email
-                    val photoUri = user?.photoUrl
-                    UserProfileContent(uri = photoUri, email = email)
-                }
+            uiState = uiState,
+            myFriends = myFriends,
+            signInWithGoogle = signInWithGoogle,
+        )
+    }
+}
 
-                else -> {
-                    UserProfileContent()
-                }
+@Composable
+private fun MyPageContent(
+    modifier: Modifier,
+    uiState: State<UiState>,
+    myFriends: State<List<MyFriend>>,
+    signInWithGoogle: (Context) -> Unit,
+) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(32.dp),
+    ) {
+        when (uiState.value) {
+            is UiState.Success -> {
+                val user = UserManager.getUser()
+                val email = user?.email
+                val photoUri = user?.photoUrl
+
+                UserProfileContent(uri = photoUri, email = email)
+
+                SocialContent(
+                    modifier = Modifier.weight(1f),
+                    myFriends = myFriends,
+                )
             }
 
-            LoginContent { signInWithGoogle(context) }
+            else -> {
+                UserProfileContent()
+                LoginContent { signInWithGoogle(context) }
+            }
         }
     }
 }
@@ -115,6 +150,7 @@ private fun UserProfileContent(uri: Uri? = null, email: String? = null) {
     Text(
         text = email ?: stringResource(R.string.my_page_default_user_email),
         modifier = Modifier.fillMaxWidth(),
+        fontWeight = FontWeight.Bold,
         textAlign = TextAlign.Center
     )
 }
@@ -156,16 +192,86 @@ private fun LoginContent(loginHandle: () -> Unit) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SocialContent(
+    modifier: Modifier,
+    myFriends: State<List<MyFriend>>,
+) {
+    var tabState by remember { mutableIntStateOf(0) }
+    val titles = listOf(
+        stringResource(R.string.my_page_social_list),
+        stringResource(R.string.my_page_social_search),
+        stringResource(R.string.my_page_social_alarm)
+    )
+
+    Column(
+        modifier = modifier
+    ) {
+        PrimaryTabRow(selectedTabIndex = tabState) {
+            titles.forEachIndexed { index, title ->
+                MyPageCustomTab(tabState, index, title) { tabState = index }
+            }
+        }
+
+        when (tabState) {
+            0 -> MyPageSocialList(myFriends.value)
+            1 -> MyPageSearch(myFriends.value)
+            2 -> MyPageAlarm(myFriends.value, {}, {})
+        }
+    }
+}
+
+@Composable
+private fun MyPageCustomTab(tabState: Int, index: Int, title: String, onClick: () -> Unit) {
+    val itemCount by remember { mutableIntStateOf(5) } // TODO : FIREBASE 알람 개수
+
+    Tab(
+        selected = tabState == index,
+        onClick = onClick,
+        text = {
+            Row {
+                Text(
+                    text = title, maxLines = 2, overflow = TextOverflow.Ellipsis
+                )
+
+                Box {
+                    if (index == 2 && itemCount > 0) {
+                        Badge(
+                            modifier = Modifier.padding(start = 8.dp),
+                            containerColor = Color.Red,
+                            contentColor = Color.White
+                        ) {
+                            Text("$itemCount")
+                        }
+                    }
+                }
+            }
+        }
+    )
+}
+
+
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Preview(showBackground = true, uiMode = UI_MODE_NIGHT_NO)
 @Composable
 private fun MyPageScreenPreview() {
     val uiState = remember { mutableStateOf(UiState.Idle) }
+    val myFriends = remember {
+        mutableStateOf(
+            listOf(
+                MyFriend("", "grand2181@gmail.com", true),
+                MyFriend("", "도윤@gmail.com", true),
+                MyFriend("", "정호@gmail.com", true)
+            )
+        )
+    }
+
     NatureAlbumTheme {
         MyPageScreen(
-            context = LocalContext.current,
             navigateToHome = {},
             uiState = uiState,
+            myFriends = myFriends,
             signInWithGoogle = {}
         )
     }
