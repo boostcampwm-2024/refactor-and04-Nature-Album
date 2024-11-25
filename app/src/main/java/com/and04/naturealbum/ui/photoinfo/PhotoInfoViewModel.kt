1ac563ee716b840c8405f5640c59254eb7f7a23e
kim4152 <andykimjhjh@gmail.com>
2024-11-25 22:24:45 +0900

    [Refactor] viewmodel리팩토링

--- app/src/main/java/com/and04/naturealbum/ui/photoinfo/PhotoInfoViewModel.kt
+++ app/src/main/java/com/and04/naturealbum/ui/photoinfo/PhotoInfoViewModel.kt
@@ -8,7 +8,6 @@ import com.and04.naturealbum.data.room.Label
 import com.and04.naturealbum.data.room.PhotoDetail
 import com.and04.naturealbum.ui.savephoto.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -38,27 +37,28 @@ class PhotoInfoViewModel @Inject constructor(
             val photoDetailData = roomRepository.getPhotoDetailById(id)
             _photoDetail.emit(photoDetailData)
 
-            val coords = "${photoDetailData.longitude}%2C${photoDetailData.latitude}"
-            convertCoordsToAddress(coords = coords)
+            convertCoordsToAddress(photoDetail = photoDetailData)
 
             _label.emit(roomRepository.getLabelById(photoDetailData.labelId))
             _uiState.emit(UiState.Success)
         }
     }
 
-    private fun convertCoordsToAddress(coords: String) {
-        viewModelScope.launch {
-            reverseGeocodeRepository.convertCoordsToAddress(coords = coords)
-                .onSuccess { dto ->
-                    val region = dto.results?.get(0)?.region
-                    val address = buildString {
-                        append("${region?.area1?.name} ")
-                        append("${region?.area2?.name} ")
-                        append("${region?.area3?.name} ")
-                        append(region?.area4?.name)
-                    }
-                    _address.emit(address)
+    private suspend fun convertCoordsToAddress(photoDetail: PhotoDetail) {
+        val coords = "${photoDetail.longitude}%2C${photoDetail.latitude}"
+        reverseGeocodeRepository.convertCoordsToAddress(coords = coords)
+            .onSuccess { dto ->
+                val region = dto.results?.get(0)?.region
+                val address = buildString {
+                    append("${region?.area1?.name} ")
+                    append("${region?.area2?.name} ")
+                    append("${region?.area3?.name} ")
+                    append(region?.area4?.name)
                 }
-        }
+                _address.emit(address)
+            }
+            .onFailure {
+                _address.emit("${photoDetail.latitude}, ${photoDetail.longitude}")
+            }
     }
 }
