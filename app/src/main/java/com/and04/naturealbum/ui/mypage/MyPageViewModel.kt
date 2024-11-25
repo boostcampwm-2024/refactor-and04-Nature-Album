f0ba08784ed6c3b0b502fc38627184298fa41554
lh99j <grand2181@gmail.com>
2024-11-25 22:16:18 +0900

    [Refactor] MyPageScreen UiState 수정

--- app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/and04/naturealbum/ui/mypage/MyPageViewModel.kt
@@ -4,7 +4,8 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.and04.naturealbum.data.dto.MyFriend
-import com.and04.naturealbum.ui.savephoto.UiState
+import com.and04.naturealbum.ui.model.UiState
+import com.and04.naturealbum.ui.model.UserInfo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,44 +17,29 @@ import javax.inject.Inject
 class MyPageViewModel @Inject constructor(
     private val authenticationManager: AuthenticationManager
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow<UiState>(setInitUiState())
-    val uiState: StateFlow<UiState> = _uiState
+    private val _uiState = MutableStateFlow<UiState<UserInfo>>(UiState.Idle)
+    val uiState: StateFlow<UiState<UserInfo>> = _uiState
 
     private val _myFriends = MutableStateFlow<List<MyFriend>>(emptyList())
     val myFriend: StateFlow<List<MyFriend>> = _myFriends
-
-    private val _userEmail = MutableStateFlow<String?>(null)
-    val userEmail: StateFlow<String?> = _userEmail
-
-    private val _userPhotoUrl = MutableStateFlow<String?>(null)
-    val userPhotoUrl: StateFlow<String?> = _userPhotoUrl
-
-    private val _userDisplayName = MutableStateFlow<String?>(null)
-    val userDisplayName: StateFlow<String?> = _userDisplayName
-
-    private val _userUid = MutableStateFlow<String?>(null)
-    val userUid: StateFlow<String?> = _userUid
 
     fun signInWithGoogle(context: Context) {
         authenticationManager.signInWithGoogle(context).onEach { response ->
             when (response) {
                 is AuthResponse.Success -> {
                     val user = UserManager.getUser()
-                    _userEmail.value = user?.email
-                    _userPhotoUrl.value = user?.photoUrl.toString()
-                    _userDisplayName.value = user?.displayName
-                    _userUid.value = user?.uid
-                    _uiState.emit(UiState.Success)
+                    _uiState.emit(
+                        UiState.Success(
+                            UserInfo(
+                                userEmail = user?.email,
+                                userPhotoUri = user?.photoUrl.toString(),
+                                userDisplayName = user?.displayName,
+                                userUid = user?.uid
+                            )
+                        )
+                    )
                 }
             }
         }.launchIn(viewModelScope)
-    }
-
-    private fun setInitUiState(): UiState {
-        return if (UserManager.isSignIn()) {
-            UiState.Success
-        } else {
-            UiState.Idle
-        }
     }
 }
