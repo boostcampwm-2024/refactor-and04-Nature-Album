5fefb0874b21353b034f6c35050696151c41c8e7
lh99j <grand2181@gmail.com>
2024-11-25 22:14:56 +0900

    [Refactor] UiState model 패키지로 분리 및 Success에 제네릭 타입의 data 생성

---
+++ app/src/main/java/com/and04/naturealbum/ui/model/UiState.kt
@@ -0,0 +1,7 @@
+package com.and04.naturealbum.ui.model
+
+sealed class UiState<out T> {
+    data object Idle : UiState<Nothing>()
+    data object Loading : UiState<Nothing>()
+    data class Success<T>(val data: T) : UiState<T>()
+}
