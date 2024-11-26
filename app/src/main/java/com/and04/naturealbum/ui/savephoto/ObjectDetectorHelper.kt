81defba38c4aecf9a11e7ac7aca7be6bfb8d1709
lh99j <grand2181@gmail.com>
2024-11-26 13:24:56 +0900

    [Refactor] 매직넘버 상수로 관리

--- app/src/main/java/com/and04/naturealbum/ui/savephoto/ObjectDetectorHelper.kt
+++ app/src/main/java/com/and04/naturealbum/ui/savephoto/ObjectDetectorHelper.kt
@@ -11,10 +11,11 @@ class ObjectDetectorHelper(context: Context, modelPath: String) {
     private val detector: ObjectDetector
 
     init {
-        val modelFile = copyAssetToInternalStorage(context, "EfficientDet.tflite", "EfficientDet.tflite")
+        val modelFile =
+            copyAssetToInternalStorage(context, "EfficientDet.tflite", "EfficientDet.tflite")
         val options = ObjectDetector.ObjectDetectorOptions.builder()
-            .setMaxResults(5) // 최대 5개의 결과만 반환
-            .setScoreThreshold(0.5f) // 50% 이상의 신뢰도만 반환
+            .setMaxResults(RESULT_COUNT) // 최대 5개의 결과만 반환
+            .setScoreThreshold(SCORE_THRES_HOLD) // 50% 이상의 신뢰도만 반환
             .build()
         detector = ObjectDetector.createFromFileAndOptions(modelFile, options)
     }
@@ -23,16 +24,26 @@ class ObjectDetectorHelper(context: Context, modelPath: String) {
         val tensorImage = TensorImage.fromBitmap(bitmap)
         return detector.detect(tensorImage)
     }
-}
 
-fun copyAssetToInternalStorage(context: Context, assetFileName: String, outputFileName: String): File {
-    val outputFile = File(context.filesDir, outputFileName)
-    if (!outputFile.exists()) {
-        context.assets.open(assetFileName).use { inputStream ->
-            outputFile.outputStream().use { outputStream ->
-                inputStream.copyTo(outputStream)
+    private fun copyAssetToInternalStorage(
+        context: Context,
+        assetFileName: String,
+        outputFileName: String
+    ): File {
+        val outputFile = File(context.filesDir, outputFileName)
+        if (!outputFile.exists()) {
+            context.assets.open(assetFileName).use { inputStream ->
+                outputFile.outputStream().use { outputStream ->
+                    inputStream.copyTo(outputStream)
+                }
             }
         }
+        return outputFile
     }
-    return outputFile
+
+    companion object {
+        private const val RESULT_COUNT = 5
+        private const val SCORE_THRES_HOLD = 0.9f
+    }
 }
+
