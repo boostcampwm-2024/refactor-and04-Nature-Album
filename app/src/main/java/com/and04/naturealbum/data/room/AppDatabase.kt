8547ccee31884cc4a30b79397e3770a4e298c0b0
kim4152 <andykimjhjh@gmail.com>
2024-12-02 12:17:29 +0900

    [Fix] room 버전 정보 수정

--- app/src/main/java/com/and04/naturealbum/data/room/AppDatabase.kt
+++ app/src/main/java/com/and04/naturealbum/data/room/AppDatabase.kt
@@ -27,7 +27,7 @@ val MIGRATION_3_4 = object : Migration(3, 4) {
 
 @Database(
     entities = [Label::class, Album::class, PhotoDetail::class],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 
