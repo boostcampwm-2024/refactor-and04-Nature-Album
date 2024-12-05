ab32b5c516fea71b25c5df9caf22ec94650651f3
lh99j <grand2181@gmail.com>
2024-12-05 10:20:34 +0900

    [Build] 배포 버전

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.and04.naturealbum"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
