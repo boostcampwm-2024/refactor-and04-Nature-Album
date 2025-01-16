21e968d7eeb34f790c216de5a9147f1af9261bf3
Yujin Jeong <yujin45@sookmyung.ac.kr>
2025-01-16 16:33:12 +0900

    [Refactor] 패키지 import 수정

--- app/src/main/java/com/and04/naturealbum/ui/friend/navigation/FriendSearchNavigation.kt
+++ app/src/main/java/com/and04/naturealbum/ui/friend/navigation/FriendSearchNavigation.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import com.and04.naturealbum.ui.friend.FriendSearchScreen
+import com.and04.naturealbum.ui.mypage.friendsearch.FriendSearchScreen
 import com.and04.naturealbum.ui.navigation.NatureAlbumNavigator
 import com.and04.naturealbum.ui.navigation.NavigateDestination
 
