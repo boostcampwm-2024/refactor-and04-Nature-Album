c90374339f631ce8d964d0e4984d60391006d1bd
kimdoyun98 <rladlsxo1250@gmail.com>
2025-01-21 16:41:03 +0900

    [Refactor] sealed interface로 변경
    BinarySearch에서 Sync 타입으로 받기 위한 용도이기에 class보단 interface가 적합하다고 판단

--- app/src/main/java/com/and04/naturealbum/data/dto/RoomDataDto.kt
+++ app/src/main/java/com/and04/naturealbum/data/dto/RoomDataDto.kt
@@ -13,7 +13,7 @@ data class SyncAlbumsDto(
     val labelBackgroundColor: String,
     val photoDetailUri: String,
     val fileName: String
-): Sync()
+): Sync
 
 data class SyncPhotoDetailsDto(
     val photoDetailUri: String,
@@ -23,6 +23,6 @@ data class SyncPhotoDetailsDto(
     val latitude: Double,
     val description: String,
     val datetime: String
-): Sync()
+): Sync
 
-sealed class Sync
+sealed interface Sync
